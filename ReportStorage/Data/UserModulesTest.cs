3c1a8ff10bc59b21bb3939504257104e0c7de3a7
IKrivokon <i.krivokon@samsung.com>
2023-03-30 16:34:54 +0300

    Initial commit

---
+++ ReportStorage/Data/UserModulesTest.cs
@@ -0,0 +1,118 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Data.SqlClient;
+
+namespace ReportStorage.Data
+{
+    public class UserModulesTest
+    {
+        public int id { get; set; }
+        public string? created_by { get; set; }
+        public string? checkbox { get; set; }
+        public string? dropdown { get; set; }
+        public string? number { get; set; }
+        public string? textbox { get; set; }
+    }
+
+    public class UserModulesField
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string? Value { get; set; }
+        public bool IsChanged { get; set; }
+        public UserModulesField() {
+            IsChanged = false;
+            Id = 0;
+            Name = "";
+            Type = "TEXT"; //default
+            Value = "";
+        }
+    }
+
+    public class UserModulesHeaderFields : UserModulesField
+    {
+        public string SortOrder { get; set; }   
+        public string Filter { get; set; }  
+        }
+
+    public class UserModulesRow
+    {
+        public int Id { get; set; }
+        public bool IsUpdated { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<UserModulesField> Fields { get; set; }
+        public  UserModulesRow()
+        {
+            Fields = new List<UserModulesField>();  
+            IsUpdated = false;
+            IsDeleted = false;
+            Id = 0;
+        }
+
+
+        public void Select() { }
+        public void Update() { }    
+        public void Delete() { }    
+    }
+
+
+    public class UserModulesTable
+    {
+        public string ConString { get; set; }   
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public DateTime LastUpdated { get; }
+        public List<String> Columns { get; set; }
+        public List<UserModulesHeaderFields> HeaderRow { get; set; }    
+        public List<UserModulesRow> Rows { get; set;}
+
+        public UserModulesTable(string name, string constring) {
+            Name = name;
+            ConString = constring;
+            Columns = new List<string>();
+            HeaderRow = new List<UserModulesHeaderFields>();
+            Rows = new List<UserModulesRow>();
+            Id = 0;
+        }
+        public void Select() { 
+            using(SqlConnection con = new SqlConnection(ConString))
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT TOP 100 * FROM " + Name;
+                SqlDataReader reader = cmd.ExecuteReader(); 
+                while (reader.Read()) {
+                    UserModulesRow row = new()
+                    {
+                        Id = reader.GetInt32(0)
+                    };
+                    UserModulesField FieldId = new()
+                    {
+                        Name = "ID",
+                        Value = row.Id.ToString()
+                    };
+                    row.Fields.Add(FieldId);
+                    for (int i = 1; i < reader.FieldCount - 1; i++)
+                    {
+                        UserModulesField field = new()
+                        {
+                            Name = reader.GetName(i),
+                            Value = reader.GetString(i),
+                            Id = row.Id
+                        };
+                        row.Fields.Add(field);
+                    }
+                    Rows.Add(row);
+                }
+                reader.Close();
+            }
+        }
+        public void Update() { }    
+        public void Delete() { }    
+        public void Insert(UserModulesRow NewRow)
+        {
+
+        }
+        
+    }
+}
---
+++ ReportStorage/Data/UserModulesTestService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ReportStorage.Data
+{
+    public class UserModulesTestService
+    {
+        private string connectionString = @"Data Source=106.109.72.69; 
+                Initial Catalog=UserModules;
+                Integrated Security=false; 
+                User ID=sa; Password=OrchestraSQL;
+                Encrypt=True;
+                TrustServerCertificate=True";
+        public Task<UserModulesTest[]> GetDataTest()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                }
+                catch (SqlException ex)
+                {
+
+                    throw;
+                }
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT top 3 * FROM UserModules.[user].TEST";
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<UserModulesTest> result = new List<UserModulesTest>();
+                while(reader.Read()) {
+                    result.Add(
+                        new UserModulesTest()
+                        {
+                            id = reader.GetInt32(0),
+                            created_by = reader.GetString(1),
+                            checkbox = reader.GetString(2),
+                            dropdown = reader.GetString(3),
+                            number = reader.GetString(4),
+                            textbox = reader.GetString(5)
+                        });
+                }
+                return Task.FromResult(result.ToArray());
+            }
+        }
+    }
+
+    public class UserModulesService
+    {
+        private string ConnectionString = @"Data Source=106.109.72.69; 
+                Initial Catalog=UserModules;
+                Integrated Security=false; 
+                User ID=sa; Password=OrchestraSQL;
+                Encrypt=True;
+                TrustServerCertificate=True";
+        
+        public Task<UserModulesTable> GetData()
+        {
+            UserModulesTable table = new UserModulesTable("[UserModules].[user].[TEST]", ConnectionString);
+            table.ConString = ConnectionString;
+            table.Select();
+            //return Task.FromResult(table.Rows.ToArray());   
+            return Task.FromResult(table);
+        }
+    }
+}
